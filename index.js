5a197466ae78cec1ec2c97e3e21621cca3493698
Kevin Peery <kevin.peery@gmail.com>
2018-04-06 01:24:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function theBeatlesPlay (musicians, instruments){
   var arr = []
   for(let i = 0; i < musicians.length; i++){
     let str = musicians[i] + " plays " + instruments[i];
+    return arr.push(str)
   }
 }
